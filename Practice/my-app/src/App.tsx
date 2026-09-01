aff79c630e7f35053649b024fe244e940545b5cb
MehmoodAhmed006 <busymehmood@gmail.com>
2026-09-02 02:17:58 +0500

    Learning useEffect

--- Practice/my-app/src/App.tsx
+++ Practice/my-app/src/App.tsx
@@ -1,9 +1,19 @@
-
-
+import * as React from "react";
+import {useEffect} from "react";
 const App = () => {
+    const [count, setCount] = React.useState<number>(0)
 
+    useEffect(() => {
+      console.log("Count Increases...")
+    }, [count])
   return (
+      <>
+        <h1>{count}</h1>
 
+        <button onClick={() => setCount(count + 1)}>
+          Increase
+        </button>
+      </>
   );
 }
 
