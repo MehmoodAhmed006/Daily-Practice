b98db89cbbbdd697c55e8e5f3c6d574346998f0d
MehmoodAhmed006 <busymehmood@gmail.com>
2026-09-03 04:07:26 +0500

    .

--- Practice/Day-1/vite-project/src/App.tsx
+++ Practice/Day-1/vite-project/src/App.tsx
@@ -1,28 +1,9 @@
-import {useMemo, useState} from 'react'
 function App() {
 
-  const [count, setCount] = useState<number>(0);
-  const [name, setName] = useState<string>('');
-
-  const handleAdd = () => {
-    setCount(prevState => prevState + 1);
-  }
-  const handleReset = () => {
-    setCount(0);
-  }
-
-  const result = useMemo(() => {
-    return expensiveCalculation(count);
-  }, [count]);
-
   return (
-    <>
-      <h1>{count}</h1>
-      <button onClick={handleAdd}>Increase</button>
-      <button onClick={handleReset}>Reset</button>
+    <div>
 
-
-    </>
+    </div>
   )
 }
 
