763443cf762fefb9cb85319aa20f9b9467d60331
MehmoodAhmed006 <busymehmood@gmail.com>
2026-09-02 11:14:06 +0500

    Learning useMemo

--- Practice/my-app/src/App.tsx
+++ Practice/my-app/src/App.tsx
@@ -1,18 +1,50 @@
 import * as React from "react";
 import {useEffect} from "react";
+
+interface NamesProps  {
+    name: string;
+    role: string;
+}
+
+function ChangeNames ({name, role}: NamesProps) {
+    return (
+     <div>
+         <h2>{name}</h2>
+         <p>{role}</p>
+     </div>
+    )
+}
+
 const App = () => {
     const [count, setCount] = React.useState<number>(0)
+    const [name, setName] = React.useState<string>('')
+    const [role, setRole] = React.useState<string>('')
 
     useEffect(() => {
-      console.log("Count Increases...")
+    const interval = setInterval(() => {
+        console.log("interval")
+    }, 1000)
+        return () => {
+        clearInterval(interval)
+        }
+
     }, [count])
+
+    const handleChange = () => {
+        setName(name === "Mehmood" ? "Yahya" : "Mehmood");
+        setRole(role === "Developer" ? "Designer" : "Developer");
+    };
+
+
   return (
       <>
-        <h1>{count}</h1>
+        <h2>count is: {count}'s</h2>
+        <button onClick={() => setCount(count + 1)}>Increase</button>
+        <button onClick={() => setCount(count - 1)}>Decrease</button>
+        <button onClick={() => setCount(0)}> Reset </button>
 
-        <button onClick={() => setCount(count + 1)}>
-          Increase
-        </button>
+        <ChangeNames name={name} role={role} />
+          <button onClick={handleChange}>Change Name</button>
       </>
   );
 }
